5f83e3271cc2f184ef0eb5e1ea8ffebb74acdc24
Roman Batavi <romanbatavi98@gmail.com>
2021-09-14 13:20:59 +0700

    lap2

--- adminit/inputnilai.php
+++ adminit/inputnilai.php
@@ -3,14 +3,10 @@
 		<div class="col-12">
 			<div class="portlet">
 				<div class="portlet-header portlet-header-bordered">
-						<h3 class="portlet-title">Data Penilaian Beasiswa PM</h3>
-						</div>
-						<div class="portlet-body">
-							<!-- <p>
-								In this example you can see Datatable doing both horizontal and vertical scrolling at the same time. To enable y-scrolling or x-scrolling simply set the <code>scrollY|scrollX</code> parameter to be whatever you want the container wrapper's height or width.
-							</p>
-							<hr> -->
-						<div class="input-group">
+					<h3 class="portlet-title">Data Penilaian Beasiswa PM</h3>
+				</div>
+				<div class="portlet-body">
+					<div class="input-group">
 						<div class="input-group-prepend">
 							<label class="input-group-text" for="inputGroupSelect01">Pilih Aspek Penilaian</label>
 						</div>
@@ -22,7 +18,6 @@
 							<option value="?m=inputnilai_eks">Ekstrakulikuler</option>
 							<option value="?m=inputnilai_skp">Sikap</option>
 							<option value="?m=inputnilai_pre">Prestasi</option>
-
 						</select>
 					</div>
 				</div>
--- adminit/laphpm.php
+++ adminit/laphpm.php
@@ -44,45 +44,40 @@ $pdf->SetFont('Arial','',10);
 
 
 $query = "SELECT user.id_user, user.nama_lengkap, user.hak_akses, user.angkatan,user.nis,
-						pm_penilaian.nilai_tot_A1 as PE, pm_kemampuan.nilai_tot_A2 as KE,
-						(((pm_penilaian.nilai_tot_A1*60)/100)+((pm_kemampuan.nilai_tot_A2*40)/100)) as Total
-						FROM user 
-						LEFT JOIN pm_penilaian ON user.id_user = pm_penilaian.id_user
-						LEFT JOIN pm_kemampuan ON user.id_user = pm_kemampuan.id_user
-						where user.hak_akses = 'siswa' ORDER BY Total DESC";
-            $hasil = $kon->query("$query");
+	pm_penilaian.nilai_tot_A1 as PE, pm_kemampuan.nilai_tot_A2 as KE,
+	(((pm_penilaian.nilai_tot_A1*60)/100)+((pm_kemampuan.nilai_tot_A2*40)/100)) as Total
+	FROM user 
+	LEFT JOIN pm_penilaian ON user.id_user = pm_penilaian.id_user
+	LEFT JOIN pm_kemampuan ON user.id_user = pm_kemampuan.id_user
+	where user.hak_akses = 'siswa' ORDER BY Total DESC";
+    $hasil = $kon->query("$query");
 
-            $no = 1;
-            while ($row = $hasil->fetch_array()) {
-            $id_user    = $row['id_user'];
-            $nama_lengkap  = $row['nama_lengkap'];
-            $nis  = $row['nis'];
+    $no = 1;
+    while ($row = $hasil->fetch_array()) {
+    $id_user    = $row['id_user'];
+    $nama_lengkap  = $row['nama_lengkap'];
+    $nis  = $row['nis'];
 
-	$pdf->Cell(1, 0.8, $no , 1, 0, 'C');
+    $pdf->Cell(1, 0.8, $no , 1, 0, 'C');
     $pdf->Cell(4, 0.8, $nis,1, 0, 'C');
     $pdf->Cell(3, 0.8, $nama_lengkap,1, 0, 'C');
-	$pdf->Cell(3, 0.8, $row['PE'],1, 0, 'C');
+    $pdf->Cell(3, 0.8, $row['PE'],1, 0, 'C');
     $pdf->Cell(4, 0.8, $row['KE'],1, 0, 'C');
     $pdf->Cell(3, 0.8, number_format((float)$row['Total'], 2, '.', ''),1, 0, 'C');
     $pdf->Cell(2, 0.8, $no,1, 1, 'C');
-	$no++;
-	
-}
-$pdf->ln(1);
-$pdf->SetFont('Arial','B',12);
-$pdf->Cell(40.5,0.7,"Tanggal: ".date("d/m/Y"),0,0,'C');
+    $no++;
 
-$pdf->ln(1);
-$pdf->SetFont('Arial','B',12);
-$pdf->Cell(40.5,0.7,"Mengetahui",0,10,'C');
-
-$pdf->ln(1);
-$pdf->ln(1);
-$pdf->SetFont('Arial','B',12);
-$pdf->Cell(40.5,0.7,"SMK DINAMIKA PEMBANGUNAN 1 JAKARTA",0,10,'C');
-
-
-$pdf->Output("LAPORAN_HASIL_PM.pdf","I");
-
-?>
+        }
+    $pdf->ln(1);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(40.5,0.7,"Tanggal: ".date("d/m/Y"),0,0,'C');
+    $pdf->ln(1);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(40.5,0.7,"Mengetahui",0,10,'C');
+    $pdf->ln(1);
+    $pdf->ln(1);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(40.5,0.7,"SMK DINAMIKA PEMBANGUNAN 1 JAKARTA",0,10,'C');
+    $pdf->Output("LAPORAN_HASIL_PM.pdf","I");
+        ?>
 
